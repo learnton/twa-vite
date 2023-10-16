30adcd9935aaa6b13ba57d9f92ee0d5a04ad180c
refined-x <tower1229@foxmail.com>
2023-10-16 11:11:37 +0800

    fix: show params

--- src/main.tsx
+++ src/main.tsx
@@ -37,15 +37,6 @@ const router = createBrowserRouter(
 
 WebApp.ready();
 
-const hash = window.location.hash.slice(1);
-const params = new URLSearchParams(hash);
-const initData = new URLSearchParams(params.get("tgWebAppData") || "");
-sessionStorage.setItem("initData", JSON.stringify(initData));
-// query_id: 'abcdef'
-// user: `{id: 1, first_name: "Peris", last_name: "Hilton", username: "peris", language_code: "en", is_premium: true}`
-// auth_date: '1669670292'
-// hash: '4975e881a0347264512f6047e1f3d698cbd2fe0bc1573'
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -2,13 +2,16 @@ import { useNavigate } from "react-router-dom";
 
 function Info() {
   const navigate = useNavigate();
-  const initData = sessionStorage.getItem("initData");
+  const hash = window.location.hash.slice(1);
+  const params = new URLSearchParams(hash);
+  const initData = new URLSearchParams(params.get("tgWebAppData") || "");
 
   return (
     <>
       <h1>test info</h1>
-      <div>{initData}</div>
-      <iframe height={300} src="https://m.baidu.com"></iframe>
+      <div>hash: {hash}</div>
+      <div>initData:{JSON.stringify(initData)}</div>
+      {/* <iframe height={300} src="https://m.baidu.com"></iframe> */}
       <div>
         <button onClick={() => navigate("/")}>back</button>
       </div>
