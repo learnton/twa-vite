b6c8d7ba4d669d3042aef6d477a64c15e4405372
refined-x <tower1229@foxmail.com>
2023-10-16 13:50:28 +0800

    feat: show WebAppData

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -5,10 +5,12 @@ function Info() {
   const hash = window.location.hash.slice(1);
   const params = new URLSearchParams(hash);
   const initData = new URLSearchParams(params.get("tgWebAppData") || "");
+  const WebAppData = (window.global as any).Telegram?.WebApp;
 
   return (
     <>
       <h1>test info</h1>
+      <div>{JSON.stringify(WebAppData)}</div>
       <div>hash: {hash}</div>
       <div>initData:{JSON.stringify(initData)}</div>
       {/* <iframe height={300} src="https://m.baidu.com"></iframe> */}
